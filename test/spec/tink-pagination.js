97da855abc343d40c75f107e1946534266393417
vincentbouillart <vincentbouillart@live.be>
2015-12-10 15:54:05 +0100

    adding unit tests

--- test/spec/tink-pagination.js
+++ test/spec/tink-pagination.js
@@ -1,5 +1,7 @@
 'use strict';
-describe('tink-interactive-table-angular', function() {
+describe('tink-pagination-angular', function() {
+
+  beforeEach(module('tink.pagination'));
 
   var bodyEl = $('body'), sandboxEl;
   var $compile, $templateCache, scope;
@@ -12,6 +14,9 @@ describe('tink-interactive-table-angular', function() {
     $templateCache = _$templateCache_;
     bodyEl.html('');
     sandboxEl = $('<div>').attr('id', 'sandbox').appendTo(bodyEl);
+
+    var template = $templateCache.get('src/templates/pagination.html');
+    $templateCache.put('templates/pagination.html',template);
   }));
 
   afterEach(function() {
@@ -30,14 +35,16 @@ describe('tink-interactive-table-angular', function() {
 
   var templates = {
     'default': {
-      scope: {},
-      element: ''
+      scope: {nums:1,numpp:5,totalitems:100},
+      element: '<tink-pagination  tink-current-page="nums" tink-change="changed(page,perPage,next)" tink-total-items="totalitems" tink-items-per-page="numpp"></tink-pagination>'
     }
   };
 
 
   describe('default', function() {
     it('should run this basic setup',function(){
+      var element = compileDirective('default');
+      console.log(element);
     });
   });
 
